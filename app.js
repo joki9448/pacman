770fef521f1619bf76210bde04389f3ad72ee5f7
John Kim <joki9448@gmail.com>
2022-11-05 14:54:46 -0400

    base styling completed

--- app.js
+++ app.js
@@ -34,4 +34,34 @@ document.addEventListener('DOMContentLoaded', () => {
         1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1    
     ]
 
+    // 0 - pac-dot
+    // 1 - wall
+    // 2 - ghost-lair
+    // 3 - power-pellet
+    // 4 - empty 
+
+    const squares = [];
+
+    function createBoard() {
+        for (let i = 0; i < layout.length; i++) {
+            const square = document.createElement('div');
+            grid.appendChild(square)
+            squares.push(square)
+
+            if (layout[i] === 0) {
+                squares[i].classList.add('pac-dot')
+            } else if (layout[i] === 1) {
+                squares[i].classList.add('wall')
+            } else if (layout[i] === 3) {
+                squares[i].classList.add('power-pellet')
+            }
+        }
+    }
+    createBoard();
+
+    let pacmanCurrentIndex = 490
+
+    squares[pacmanCurrentIndex].classList.add('pac-man')
+
+    
 })
